20888ec87715ccd7a361d824ed0c387afeda74df
Ali Makhdoom <14muhammad@gmail.com>
2024-01-07 23:31:52 +0500

    mongodb setup

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
 const express = require('express');
 const app = express();
-const port = process.env.PORT || 5000 ;
+require('dotenv').config()
+const port = process.env.PORT || 5000;
+//console.log(process.env.MONGO_URL);
 app.listen(port, ()=> console.log(`Node server started at port ${port}`));
