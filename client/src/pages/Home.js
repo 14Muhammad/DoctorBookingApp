76e343568d19d79090b20a3116cb18f4fdba808b
Ali Makhdoom <14muhammad@gmail.com>
2024-01-11 22:34:03 +0500

    authorization of user with token in header

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,9 +1,23 @@
-import React from 'react';
-
+import React, {useEffect} from 'react';
+import axios from "axios";
 function Home(props) {
+    const getData = async ()=>{
+        try {
+            const response = await axios.post('/api/user/get-user-info-by-id',{}, {
+                headers: {
+                    authorization: 'Bearer ' + localStorage.getItem('token')
+                }
+            })
+            console.log(response.data);
+        } catch (e) {
+            console.log(e);
+        }
+    }
+    useEffect(()=>{
+        getData();
+    },[])
     return (
         <div>Doctor Booking App</div>
     );
 }
-
 export default Home;
--- routes/userRoute.js
+++ routes/userRoute.js
@@ -3,36 +3,37 @@ const router = express.Router();
 const User = require('../models/userModel');
 const bcrypt = require('bcrypt');
 const jwt = require("jsonwebtoken");
+const authMiddleware = require("../middlewares/authMiddleware");
 router.post('/register', async (req, res) => {
     try {
         const userExists = await User.findOne({email: req.body.email});
-        if(userExists){
+        if (userExists) {
             return res.status(400).send({message: 'User already exists', success: false});
         }
         const password = req.body.password;
-        const salt = await(bcrypt.genSalt(10));
-        const encryptedPassword = await(bcrypt.hash(password, salt));
+        const salt = await (bcrypt.genSalt(10));
+        const encryptedPassword = await (bcrypt.hash(password, salt));
         req.body.password = encryptedPassword;
         const newUser = new User(req.body);
         await newUser.save();
         res.status(200).send({message: 'User created successfully', success: true});
     } catch (e) {
-         res.status(500).send({message: 'Error creating user', success: false, e});
+        res.status(500).send({message: 'Error creating user', success: false, e});
     }
 })
 router.post('/login', async (req, res) => {
     try {
         const user = await User.findOne({email: req.body.email});
-        if(!user){
+        if (!user) {
             return res.status(200)
                 .send({message: "User does not exist", success: false});
         }
         const isMatch = await bcrypt.compare(req.body.password, user.password);
-        if(!isMatch){
+        if (!isMatch) {
             return res.status(200)
                 .send({message: "Password is incorrect", success: false});
-        }else{
-            const token = jwt.sign({ id : user._id}, process.env.JWT_SECRET, {expiresIn: "1d"});
+        } else {
+            const token = jwt.sign({id: user._id}, process.env.JWT_SECRET, {expiresIn: "1d"});
             res.status(200)
                 .send({message: "Login successful", success: true, data: token});
         }
@@ -40,6 +41,28 @@ router.post('/login', async (req, res) => {
         console.log(e);
         res.status(500)
             .send({message: "Error logging in", success: false});
+    }
+})
+
+router.post('/get-user-info-by-id', authMiddleware, async (req, res) => {
+    try {
+        const user = await User.findOne({_id: req.body.userId});
+        if (!user) {
+            return res.status(200)
+                .send({message: "User does not exist", success: false});
+        } else {
+            res.status(200).send({
+                success: true, data: {
+                    name: user.name,
+                    email: user.email
+                }
+            })
+        }
+    } catch (e) {
+        res.status(500).send({
+            message: "Error getting user info",
+            success: false, e
+        });
     }
 })
 module.exports = router;
