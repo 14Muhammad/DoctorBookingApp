c6af4ec783523110a1fdf04a0612898c45823214
Ali Makhdoom <14muhammad@gmail.com>
2024-01-11 14:38:59 +0500

    redirecting to login

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Button, Form} from "antd";
+import {Button, Form, Input} from "antd";
 import {Link} from "react-router-dom";
 
 function Login(props) {
@@ -11,11 +11,11 @@ function Login(props) {
             <div className='authentication-form card p-3'>
                 <h1 className='card-title'>Welcome Back </h1>
                 <Form layout='vertical' onFinish={onFinish}>
-                    <Form.Item label='Email' name='email' initialValue={''}>
-                        <input placeholder='Email'/>
+                    <Form.Item label='Email' name='email'>
+                        <Input placeholder='Email'/>
                     </Form.Item>
-                    <Form.Item label='Password' name='password' initialValue={''}>
-                        <input placeholder='Password' type='password'/>
+                    <Form.Item label='Password' name='password'>
+                        <Input placeholder='Password' type='password'/>
                     </Form.Item>
                     <Button className='primary-button my-2' htmlType='submit'>LOGIN</Button>
                     <Link to='/register' className='anchor'>CLICK HERE TO REGISTER</Link>
--- client/src/pages/Register.js
+++ client/src/pages/Register.js
@@ -1,36 +1,25 @@
 import React from 'react';
 import {Button, Form, Input} from "antd";
-import {Link} from "react-router-dom";
+import {Link, useNavigate} from "react-router-dom";
 import axios from "axios";
 import toast from "react-hot-toast";
 
 function Register(props) {
+    const navigate = useNavigate();
     const onFinish = async (values) => {
         try {
             console.log('Received values of the form', values);
             const response = await axios.post("/api/user/register", values);
             if (response.data.success) {
                 toast.success(response.data.message);
+                toast("Redirecting to login page");
+                navigate("/login");
             } else {
                 toast.error(response.data.message);
             }
         } catch (error) {
-            if (error.response) {
-                // The request was made and the server responded with a status code
-                // that falls out of the range of 2xx
-                console.error('Server responded with an error status:', error.response.status);
-                console.error('Error response data:', error.response.data);
-            } else if (error.request) {
-                // The request was made but no response was received
-                console.error('No response received from the server');
-            } else {
-                // Something happened in setting up the request that triggered an Error
-                console.error('Error setting up the request:', error.message);
-            }
             toast.error('Something went wrong');
-            console.log(error);
         }
-
     }
     return (
         <div className='authentication'>
@@ -53,5 +42,4 @@ function Register(props) {
         </div>
     );
 }
-
 export default Register;
