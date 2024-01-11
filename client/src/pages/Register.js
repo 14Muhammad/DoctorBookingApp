716933bb26c5bf10ee3c8e92b29c7fe5ae3c8c0b
Ali Makhdoom <14muhammad@gmail.com>
2024-01-11 14:27:22 +0500

    proxy error fixed, Input antd,

--- client/src/pages/Register.js
+++ client/src/pages/Register.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Button, Form} from "antd";
+import {Button, Form, Input} from "antd";
 import {Link} from "react-router-dom";
 import axios from "axios";
 import toast from "react-hot-toast";
@@ -8,30 +8,43 @@ function Register(props) {
     const onFinish = async (values) => {
         try {
             console.log('Received values of the form', values);
-            const response = await axios('/api/user/register', values);
+            const response = await axios.post("/api/user/register", values);
             if (response.data.success) {
                 toast.success(response.data.message);
             } else {
                 toast.error(response.data.message);
             }
-        } catch (e) {
+        } catch (error) {
+            if (error.response) {
+                // The request was made and the server responded with a status code
+                // that falls out of the range of 2xx
+                console.error('Server responded with an error status:', error.response.status);
+                console.error('Error response data:', error.response.data);
+            } else if (error.request) {
+                // The request was made but no response was received
+                console.error('No response received from the server');
+            } else {
+                // Something happened in setting up the request that triggered an Error
+                console.error('Error setting up the request:', error.message);
+            }
             toast.error('Something went wrong');
-            console.log(e);
+            console.log(error);
         }
+
     }
     return (
         <div className='authentication'>
             <div className='authentication-form card p-3'>
                 <h1 className='card-title'>Nice To Meet You </h1>
                 <Form layout='vertical' onFinish={onFinish}>
-                    <Form.Item label='Name' name='name' initialValue={''}>
-                        <input placeholder='Name' value=''/>
+                    <Form.Item label='Name' name='name'>
+                        <Input placeholder='Name' value=''/>
                     </Form.Item>
-                    <Form.Item label='Email' name='email' initialValue={''}>
-                        <input placeholder='Email'/>
+                    <Form.Item label='Email' name='email'>
+                        <Input placeholder='Email'/>
                     </Form.Item>
-                    <Form.Item label='Password' name='password' initialValue={''}>
-                        <input placeholder='Password' type='password'/>
+                    <Form.Item label='Password' name='password'>
+                        <Input placeholder='Password' type='password'/>
                     </Form.Item>
                     <Button className='primary-button my-2' htmlType='submit'>REGISTER</Button>
                     <Link to='/login' className='anchor'>CLICK HERE TO LOGIN</Link>
