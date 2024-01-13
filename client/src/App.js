098ef365e27923bfd0f2261139a0f33154861d88
Ali Makhdoom <14muhammad@gmail.com>
2024-01-13 21:25:07 +0500

    Public and Protected Routes implementation

--- client/src/App.js
+++ client/src/App.js
@@ -5,6 +5,8 @@ import Login from "./pages/Login";
 import Register from "./pages/Register";
 import Home from "./pages/Home";
 import {useSelector} from "react-redux";
+import ProtectedRoute from "./components/ProtectedRoute";
+import PublicRoute from "./components/PublicRoute";
 
 function App() {
     const {loading} = useSelector(state => state.alerts);
@@ -19,9 +21,9 @@ function App() {
                 reverseOrder={false}
             />
             <Routes>
-                <Route path='/login' element={<Login/>}/>
-                <Route path='/register' element={<Register/>}/>
-                <Route path='/' element={<Home/>}/>
+                <Route path='/login' element={<PublicRoute><Login/></PublicRoute>}/>
+                <Route path='/register' element={<PublicRoute><Register/></PublicRoute>}/>
+                <Route path='/' element={<ProtectedRoute><Home/></ProtectedRoute>}/>
             </Routes>
         </BrowserRouter>
     );
