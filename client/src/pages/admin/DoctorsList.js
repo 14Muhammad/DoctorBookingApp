e368bc89983e5ad6a5e5a0613a07254709ebf210
Ali Makhdoom <14muhammad@gmail.com>
2024-01-22 20:58:33 +0500

    users and doctors GUI in Admin Panel.

--- client/src/pages/admin/DoctorsList.js
+++ client/src/pages/admin/DoctorsList.js
@@ -1,10 +1,73 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import Layout from "../../components/Layout";
+import {useDispatch} from "react-redux";
+import {hideLoading, showLoading} from "../../redux/alertsSlice";
+import axios from "axios";
+import {Table} from "antd";
 
 function DoctorsList(props) {
+    const [doctors, setDoctors] = useState([])
+    const dispatch = useDispatch();
+    const getDoctorsData = async () => {
+        try {
+            dispatch(showLoading());
+            const response = await axios.get(
+                '/api/admin/get-all-doctors',
+                {
+                    headers: {
+                        Authorization: `Bearer ${localStorage.getItem('token')}`
+                    }
+                })
+            dispatch(hideLoading());
+            if (response.data.success) {
+                setDoctors(response.data.data)
+            }
+        } catch (e) {
+            dispatch(hideLoading());
+        }
+    }
+    useEffect(() => {
+        getDoctorsData()
+    }, [])
+
+    const columns = [
+        {
+            title: 'Name',
+            dataIndex: 'name',
+            render: (text, record) => (
+                <span className='normal-text'>
+                    {record.firstName} {record.lastName}
+            </span>
+            )
+        },
+        {
+            title: 'Phone',
+            dataIndex: 'phoneNumber'
+        },
+        {
+            title: 'Created At',
+            dataIndex: 'createdAt'
+        },
+        {
+            title: 'Status',
+            dataIndex: 'status'
+        },
+        {
+            title: 'Action',
+            dataIndex: 'action',
+            render: (text, record) => (
+                <div className='d-flex'>
+                    {record.status === "pending" && <h1 className="anchor">Approve</h1>}
+                    {record.status === "approved" && <h1 className="anchor">Block</h1>}
+                </div>
+            )
+        },
+
+    ]
     return (
         <Layout>
             <h1 className="page-title"> Doctors List</h1>
+            <Table columns={columns} dataSource={doctors}/>
         </Layout>
     );
 }
--- client/src/pages/admin/UsersList.js
+++ client/src/pages/admin/UsersList.js
@@ -1,10 +1,63 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import Layout from "../../components/Layout";
+import {useDispatch} from "react-redux";
+import {hideLoading, showLoading} from "../../redux/alertsSlice";
+import axios from "axios";
+import {Table} from "antd";
 
 function UsersList(props) {
+    const [users, setUsers] = useState([])
+    const dispatch = useDispatch();
+    const getUsersData = async () => {
+        try {
+            dispatch(showLoading());
+            const response = await axios.get(
+                '/api/admin/get-all-users',
+                {
+                    headers: {
+                        Authorization: `Bearer ${localStorage.getItem('token')}`
+                    }
+                })
+            dispatch(hideLoading());
+            if (response.data.success) {
+                setUsers(response.data.data)
+            }
+        } catch (e) {
+            dispatch(hideLoading());
+        }
+    }
+    useEffect(() => {
+        getUsersData()
+    }, [])
+
+    const columns = [
+        {
+            title: 'Name',
+            dataIndex: 'name'
+        },
+        {
+            title: 'Email',
+            dataIndex: 'email'
+        },
+        {
+            title: 'Created At',
+            dataIndex: 'createdAt'
+        },
+        {
+            title: 'Action',
+            dataIndex: 'action',
+            render: (text, record)=>(
+                <div className='d-flex'>
+                    <h1 className='anchor'>Block</h1>
+                </div>
+            )
+        },
+
+    ]
     return (
         <Layout>
             <h1 className="page-title"> Users List</h1>
+            <Table columns={columns} dataSource={users}/>
         </Layout>
     );
 }
